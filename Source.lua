c17b55d12216a8e2f0c88a9c3a630504e0e4a7a1
condorn <168012668+choy0on@users.noreply.github.com>
2026-07-13 03:36:50 +0900

    Enable RichText for Label title in ItemHold

--- Source.lua
+++ Source.lua
@@ -1148,6 +1148,7 @@ function SolarisLib:New(Config)
                 local Label, LabelFrame = {}, game:GetObjects("rbxassetid://7032552322")[1]
                 LabelFrame.Parent = Section
                 LabelFrame.Title.Text = text
+				LabelFrame.Title.RichText = true
                 LabelFrame.Name = text .. "element"
 
                 function Label:Set(tochange)
